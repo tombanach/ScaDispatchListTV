68b48df7557a0e0045841d5fc2bef766d5913f88
Tomasz Banach <tbanach@smay.eu>
2022-09-19 13:55:02 +0200

    Order Model

--- ScaDispatchListTV/Controllers/HomeController.cs
+++ ScaDispatchListTV/Controllers/HomeController.cs
@@ -1,21 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ScaDispatchListTV.Models;
 using System.Diagnostics;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
 
 namespace ScaDispatchListTV.Controllers
 {
     public class HomeController : Controller
     {
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        SqlConnection con = new SqlConnection();
+        List<Order> orders = new List<Order>();
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            con.ConnectionString = ScaDispatchListTV.Properties.Resources.ConnectionString;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        private void FetchData()
+        {
+            try
+            {
+                con.Open();
+                com.Connection = con;
+                com.CommandText = "SELECT TOP(30) Klient, Zamówienie, Nazwa, [Il.zam], Stan, ISNULL(Got, 0) Got, [Data wys] " +
+                    "FROM SmayDB.dbo.ExScaDispatchListWMS " +
+                    "WHERE ([Data wys] = CAST( GETDATE() AS Date )) " +
+                    "ORDER BY Klient";
+                dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    orders.Add(new Order() {Klient = dr["Klient"].ToString(),
+                    Zamowienie = dr["Zamówienie"].ToString(),
+                    Nazwa = dr["Nazwa"].ToString(),
+                    });
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public IActionResult Privacy()
---
+++ ScaDispatchListTV/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace ScaDispatchListTV.Models
+{
+    public class Order
+    {
+        public string Klient { get; set; }
+        public string Zamowienie { get; set; }
+        public string Nazwa { get; set; }
+        public string IlZam { get; set; }
+        public string Stan { get; set; }
+        public string Got { get; set; }
+        public string DataWys { get; set; }
+    }
+}
